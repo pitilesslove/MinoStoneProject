212930970f981f619072d2ea5c147c8ef6f4799b
computer99 <pitilesslove@gmail.com>
2022-02-27 14:49:11 +0900

    - PlayGround를 function형으로 변경 -> useState, useEffect 사용 -> 파일로 Unit데이터 가져오기

--- src/components/PlayGround.js
+++ src/components/PlayGround.js
@@ -1,103 +1,67 @@
 import '../css/common.css';
 import '../css/PlayGround.css';
 import Unit from './Unit.js'
-import React, { Component } from "react";
-import ReactDOM from 'react-dom';
+import React, { Component, useState, useEffect } from "react";
 
-class PlayGround extends Component {
+export default function PlayGround() {
 
+  const [enemy_units, setEnemyUnits] = useState([]);
+  const [my_units, setMyUnits] = useState([]);
 
+  useEffect(() => {
+    fetch("/data/units.json")
+      .then(res => res.json())
+      .then(res => {
+        setEnemyUnits(res.enemy_units);
+        setMyUnits(res.my_units);
+      })
+  }, []);
 
-  constructor(props) {
-    super(props);
+  const enemyUnits = enemy_units.map(unit => {
+    return <Unit
+      key={unit.id}
+      id={unit.id}
+      attack={unit.attack}
+      life={unit.life}
+    />
+  })
 
-    this.state = {
-      units: [
-      ]
-    };
-  }
+  const myUnits = my_units.map(unit => {
+    return <Unit
+      key={unit.id}
+      id={unit.id}
+      attack={unit.attack}
+      life={unit.life}
+    />
+  })
 
-  createMyUnits() {
-    var units = [
-      { id: 1, attack: 5, life: 6 },
-      { id: 2, attack: 3, life: 13 },
-      { id: 3, attack: 2, life: 7 },
-      { id: 4, attack: 4, life: 3 },
-      { id: 5, attack: 2, life: 4 },
-    ];
-    return units;
-  }
-
-  createEnemyUnits() {
-    var units = [
-      { id: 5, attack: 5, life: 6 },
-      { id: 4, attack: 3, life: 13 },
-      { id: 3, attack: 2, life: 7 },
-      { id: 2, attack: 4, life: 3 },
-      { id: 1, attack: 2, life: 4 },
-    ];
-    return units;
-  }
-
-
-  spawnUnits(units) {
-    console.log("spawnUnits");
-    var _unitsJsx = [];
-    for (let i = 0; i < units.length; i++) {
-      _unitsJsx.push(
-        <Unit
-          key={units[i].id}
-          id={units[i].id}
-          attack={units[i].attack}
-          life={units[i].life}
-        />
-      )
-    }
-    return _unitsJsx;
-  }
-
-  startGame(e) {
+  const startGame = (e) => {
     console.log("start game is clicked!");
-    var myUnits = this.createMyUnits();
-    var enemyUnits = this.createEnemyUnits();
-    var myUnitsJsx = this.spawnUnits(myUnits);
-    var enemyUnitsJsx = this.spawnUnits(enemyUnits);
-    var my_batch = document.getElementById("my_batch")
-    var enemy_batch = document.getElementById("enemy_batch")
-    ReactDOM.render(myUnitsJsx, my_batch);
-    ReactDOM.render(enemyUnitsJsx, enemy_batch);
   }
 
-
-  render() {
-
-
-    return (
-      <div id="playground">
-        <div id="enemy_ground" className="ground">
-          <span>적의 공간 (Enemy's ground)</span>
-          <div id="enemy_batch" className="batch_ground">
-
-          </div>
-        </div>
-        <div id="turn_area">
-          <div
-            className="turn_button"
-            onClick={this.startGame.bind(this)}
-          >
-            Start
-          </div>
-        </div>
-        <span id="front_line"></span>
-        <div id="my_ground" className="ground">
-          <span>나의 공간 (My ground)</span>
-          <div id="my_batch" className="batch_ground">
-
-          </div>
+  return (
+    <div id="playground">
+      <div id="enemy_ground" className="ground">
+        <span>적의 공간 (Enemy's ground)</span>
+        <div id="enemy_batch" className="batch_ground">
+          {enemyUnits}
         </div>
       </div>
-    );
-  }
+      <div id="turn_area">
+        <div
+          className="turn_button"
+          onClick={startGame.bind(this)}
+        >
+          Start
+        </div>
+      </div>
+      <span id="front_line"></span>
+      <div id="my_ground" className="ground">
+        <span>나의 공간 (My ground)</span>
+        <div id="my_batch" className="batch_ground">
+          {myUnits}
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default PlayGround;
